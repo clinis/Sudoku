bbf91737799d9739528654d9a89bdb530845ad94
clinis <dinisareias@gmail.com>
2016-03-16 07:11:09 +0000

    GUI com dois panel: panel superior para botões e panel central para o sudoku

--- src/GUI1.java
+++ src/GUI1.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 
@@ -20,36 +21,55 @@ public class GUI1 extends JFrame {
 
     public static final int SBORDER = 3;
 
+
     private JTextField[][] cels = new JTextField[GRID_SIZE][GRID_SIZE];
 
-    private int[][] puzzle =
+    private int[][] nums =
             {{5, 3, 4, 6, 7, 8, 9, 1, 2},
-                    {6, 7, 2, 1, 9, 5, 3, 4, 8},
-                    {1, 9, 8, 3, 4, 2, 5, 6, 7},
-                    {8, 5, 9, 7, 6, 1, 4, 2, 3},
-                    {4, 2, 6, 8, 5, 3, 7, 9, 1},
-                    {7, 1, 3, 9, 2, 4, 8, 5, 6},
-                    {9, 6, 1, 5, 3, 7, 2, 8, 4},
-                    {2, 8, 7, 4, 1, 9, 6, 3, 5},
-                    {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+            {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            {8, 5, 9, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 9}};
 
-    private boolean[][] masks =
+    private boolean[][] mascara =
             {{false, false, false, false, false, true, false, false, false},
-                    {false, false, false, false, false, false, false, false, true},
-                    {false, false, false, false, false, false, false, false, false},
-                    {false, false, false, false, false, false, false, false, false},
-                    {false, false, false, false, false, false, false, false, false},
-                    {false, false, false, false, false, false, false, false, false},
-                    {false, false, false, false, false, false, false, false, false},
-                    {false, false, false, false, false, false, false, false, false},
-                    {false, false, false, false, false, false, false, false, false}};
+            {false, false, false, false, false, false, false, false, true},
+            {false, false, false, false, false, false, false, false, false},
+            {false, false, false, false, false, false, false, false, false},
+            {false, false, false, false, false, false, false, false, false},
+            {false, false, false, false, false, false, false, false, false},
+            {false, false, false, false, false, false, false, false, false},
+            {false, false, false, false, false, false, false, false, false},
+            {false, false, false, false, false, false, false, false, false}};
 
     /**
      * Constructor to setup the game and the UI Components
      */
     public GUI1() {
-        Container cp = getContentPane();
-        cp.setLayout(new GridLayout(3, 3,5,5));
+        Container janela = getContentPane();
+        janela.setLayout(new BorderLayout());
+
+        JPanel panelTopo = new JPanel(new FlowLayout());
+        janela.add(panelTopo, BorderLayout.NORTH);
+
+        JButton button1 = new JButton("Button 1");
+        panelTopo.add(button1);
+        JButton button2 = new JButton("Button 2");
+        panelTopo.add(button2);
+        JButton button3 = new JButton("Button 3");
+        panelTopo.add(button3);
+        JButton button4 = new JButton("Button 4");
+        panelTopo.add(button4);
+
+        JPanel panelSudoku = new JPanel(new GridLayout(3, 3, 5, 5));
+        janela.add(panelSudoku, BorderLayout.CENTER);
+        //Container panelSudoku = getContentPane();
+        //panelSudoku.setLayout(new GridLayout(3, 3,5,5));
+
 
         // Allocate a common listener as the ActionEvent listener for all the
         //  JTextFields
@@ -60,30 +80,29 @@ public class GUI1 extends JFrame {
 
         panels = new JPanel[3][3];
         fields = new JTextField[3][3];
-        for (int y = 0; y < 3; y++) {
-            for (int x = 0; x < 3; x++) {
-                panels[y][x] = new JPanel(new GridLayout(3, 3));
-                panels[y][x].setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
-                //add(panels[y][x]);
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                panels[x][y] = new JPanel(new GridLayout(3, 3));
+                panels[x][y].setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
 
-                for (int yy = 0; yy < 3; yy++) {
-                    for (int xx = 0; xx < 3; xx++) {
-                        fields[yy][xx] = new JTextField();
-                        panels[y][x].add(fields[yy][xx]);
+                for (int a = 0; a < 3; a++) {
+                    for (int b = 0; b < 3; b++) {
+                        fields[a][b] = new JTextField();
+                        panels[x][y].add(fields[a][b]);
 
                         // Beautify all the cells
-                        fields[yy][xx].setHorizontalAlignment(JTextField.CENTER);
-                        fields[yy][xx].setFont(FONT_NUMBERS);
+                        fields[a][b].setHorizontalAlignment(JTextField.CENTER);
+                        fields[a][b].setFont(FONT_NUMBERS);
                     }
                 }
-                add(panels[y][x]);
+                panelSudoku.add(panels[x][y]);
             }
         }
 
 
         // Set the size of the content-pane and pack all the components
         //  under this container.
-        cp.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
+        panelSudoku.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
         pack();
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Handle window closing
